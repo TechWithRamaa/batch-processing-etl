7316072d34c63f92ee3bd93866bcd2fd6c5b3483
EngineeringWithRamaa <rkramadhevi@gmail.com>
2022-05-20 16:48:20 +0530

    ETL processing of - 10000 records of users, gets transformed as Chunks of 100 records at a time

--- src/main/java/com/engineeringwithramaa/batchexample/batch/Processor.java
+++ src/main/java/com/engineeringwithramaa/batchexample/batch/Processor.java
@@ -13,9 +13,29 @@ public class Processor implements ItemProcessor<User, User> {
     private static Map<String, String> DEPT_NAMES = new HashMap<>();
 
     public Processor() {
-        DEPT_NAMES.put("PW001","Software Engineering");
-        DEPT_NAMES.put("TX14","Research & Development");
-        DEPT_NAMES.put("MC990","IT Ops");
+        DEPT_NAMES.put("ADMIN_ASST","Administrative Assistant");
+        DEPT_NAMES.put("EXEC_ASST","Executive Assistant");
+        DEPT_NAMES.put("MKT_MGR","Marketing Manager");
+
+        DEPT_NAMES.put("CT_SR_RPT","Customer Service Representative");
+        DEPT_NAMES.put("NUR_PTNR","Nurse Practitioner");
+        DEPT_NAMES.put("SWE","Software Engineer");
+
+        DEPT_NAMES.put("SM","Sales Manager");
+        DEPT_NAMES.put("DEC","Data Entry Clerk");
+        DEPT_NAMES.put("OFF_ASS","Office Assistant");
+
+        DEPT_NAMES.put("MKG_CDR","Marketing Coordinator");
+        DEPT_NAMES.put("MED_ASST","Medical Assistant");
+        DEPT_NAMES.put("WEB_DSNR","Web Designer");
+
+        DEPT_NAMES.put("DOG_TNR","Dog Trainer");
+        DEPT_NAMES.put("POS","President of Sales");
+        DEPT_NAMES.put("NUR_ASST","Nursing Assistant");
+
+        DEPT_NAMES.put("PM","Project Manager");
+        DEPT_NAMES.put("LBRN","Librarian");
+        DEPT_NAMES.put("ACC_EXEC","Account Executive");
     }
 
     @Override
